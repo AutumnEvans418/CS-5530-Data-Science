907957378d1a91e5d2d17415575658a2f0819e05
Autumn <AutumnFox418@gmail.com>
2025-09-17 19:32:09 -0500

    finished data analysis

--- Assignment-1/Question-1/src/analyze_data.py
+++ Assignment-1/Question-1/src/analyze_data.py
@@ -4,3 +4,18 @@ import numpy as np
 df = pd.read_csv("Assignment-1\\Question-1\\clean_data\\frailty_clean.csv")
 
 print(df)
+
+summary = df.describe().to_markdown()
+
+findings = f"""
+# Findings from Data Analysis
+
+## Summary Statistics
+{summary}
+
+## Correlations
+{df[['Grip_strength_kg','Frailty_Y']].corr().to_markdown()}
+"""
+
+with open("Assignment-1\\Question-1\\reports\\findings.md", "w") as f:
+    f.write(findings)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS-5530-Data-Science
 Data Science Assignments
+
+> NOTE: Run all scripts with this folder as the current directory.
